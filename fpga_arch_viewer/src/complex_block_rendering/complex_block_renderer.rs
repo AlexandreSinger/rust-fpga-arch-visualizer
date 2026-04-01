84dc33b68f1e34120a3cfd8d5558ee4a7644d644
AlexandreSinger <alex.singer@mail.utoronto.ca>
2026-04-01 16:34:23 -0400

    Set up scaffolding for complex block rendering

--- fpga_arch_viewer/src/complex_block_rendering/complex_block_renderer.rs
+++ fpga_arch_viewer/src/complex_block_rendering/complex_block_renderer.rs
@@ -1,7 +1,19 @@
-use fpga_arch_parser::{ComplexBlockGraph, ComplexBlockNodeId};
+use fpga_arch_parser::{ComplexBlockGraph, ComplexBlockModeId, ComplexBlockNodeId};
 
 use crate::complex_block_rendering::complex_block_render_state::ComplexBlockRenderState;
 
+// This will draw the mode (i.e. the contents of the block).
+fn render_selected_mode(
+    mode_id: ComplexBlockModeId,
+    complex_block_graph: &ComplexBlockGraph,
+    state: &mut ComplexBlockRenderState,
+) -> Vec<egui::Shape> {
+    // Use a levelization scheme to place the complex blocks within the mode.
+
+    Vec::new()
+}
+
+// This will draw the overall complex block (i.e. bar on top & mode select)
 pub fn render_complex_block(
     complex_block_id: ComplexBlockNodeId,
     complex_block_graph: &ComplexBlockGraph,
